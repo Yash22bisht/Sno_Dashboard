ec043c27890c16a59dfe6c24ab5b7d88ac4bbc5b
Yash Bisht <121621281+Yash22bisht@users.noreply.github.com>
2024-10-18 18:47:11 +0530

    Update TableStatus.js

--- TableStatus.js
+++ TableStatus.js
@@ -1,3 +1,19 @@
+async function fetchData(table, studio) {
+  const url = `apis/data/${table}/${studio}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data[0];
+}
+
+function getParameterByName(name, url = window.location.href) {
+  name = name.replace(/[\[\]]/g, "\\$&");
+  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+  const results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 async function createTableWisePerformanceGraph() {
   const clubName = getParameterByName("clubname");
   const studio = getParameterByName("studio");
