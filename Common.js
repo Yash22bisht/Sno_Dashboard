8a8e194c591e62cf89dccae5c1b89c4517b9489d
Yash Bisht <121621281+Yash22bisht@users.noreply.github.com>
2024-10-19 17:13:57 +0530

    Update Common.js

--- Common.js
+++ Common.js
@@ -1,1 +1,20 @@
+async function fetchData(table, Studio) {
+    const url = ` https://app.snookerplus.in/apis/data/${table}/${studio}`;
+    console.log('Fetching data from:', url);
 
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log('Fetched data:', data);
+
+        return data[0];  // Flatten the nested arrays into a single array
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return []; // Return an empty array or handle the error as needed
+    }
+}
